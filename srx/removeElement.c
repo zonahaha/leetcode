7a049ce0b5088c93c3742b7dfa8aea00ac9b7e8f
zonahaha <katherjing@gmail.com>
2018-04-16 15:01:36 +0800

    Create removeElement.c

---
+++ srx/removeElement.c
@@ -0,0 +1,8 @@
+int removeElement(int* nums, int numsSize, int val) {
+    int count =0;
+    for(int i=0;i<numsSize;i++){
+        if(nums[i]!=val)
+            nums[count++]=nums[i];
+    }
+    return count;
+}

f6081a8a048cc484890718c59e2115ca7e549374
zonahaha <katherjing@gmail.com>
2018-04-19 17:46:50 +0800

    Update searchInsert.c
    二分法写的太复杂了！！！！人家就8行！！

--- srx/searchInsert.c
+++ srx/searchInsert.c
@@ -13,3 +13,35 @@ int searchInsert(int* nums, int numsSize, int target) {
     }
     return res;
 }
+//////////////////////////////////////////////////////////第二种解法：二分法
+int searchInsert2(int *nums, int numsSize, int target) {//二分法解决
+    int i = 0;
+    int res = 0;
+    int h = 0;
+    h = numsSize;
+    if (target > nums[h - 1]) {
+        return h;
+    } else if (target == nums[h - 1]) {
+        return h - 1;
+    } else if (target <= nums[0]) {
+        return 0;
+    }
+    i = (h + i) / 2;
+    h = h / 2;
+    while (nums[i] && h >= 1) {
+        h = h / 2;
+        if (nums[i] > target) {//左边
+            i = i - h;
+            continue;
+        } else if (nums[i] == target) {
+            res = i;
+            return res;
+        } else {//右边
+            i = i + h;
+        }
+    }
+    if (nums[i - 1] == target)return i - 1;
+    else if (nums[i + 1] == target)return i + 1;
+    else res = nums[i] > target ? i : i + 1;
+    return res;
+}

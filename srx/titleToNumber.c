292498ea564501dde5bde327f473eb9e763161d5
zonahaha <katherjing@gmail.com>
2018-06-05 17:47:09 +0800

    Update titleToNumber.c

--- srx/titleToNumber.c
+++ srx/titleToNumber.c
@@ -5,3 +5,12 @@ int titleToNumber(char* s) {
     return res;
        
 }
+int titleToNumber(char* s) {
+    int i=0;
+    int res=0;
+    while(s[i]){
+        res=26*res+s[i++]-64;//两种方法的区别是如何把字符串转换成整型数字，一个是-64，一个是-'0'-16，字符0对应的是49
+    }
+    return res;
+       
+}

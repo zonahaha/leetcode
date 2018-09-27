d9bf4e9068ae8bd595e42e3a05c4f047800aa7a3
zonahaha <katherjing@gmail.com>
2018-09-27 15:04:49 +0800

    Update printKMax.cpp

--- srx/printKMax.cpp
+++ srx/printKMax.cpp
@@ -10,10 +10,10 @@ vector<int> printKMax(int arr[], int n, int k){//
   for(int i=0;i<n;i++){
     while(!q.empty()&&arr[q.back()]<=arr[i])q.pop_back();
     q.push_back(i);
-    if(q.pop()==i-k)q.pop_front();
-    if(i>=k-1)res.push(arr[q.front()]);
+    if(q.front()==i-k)q.pop_front();
+    if(i>=k-1)res.push_back(arr[q.front()]);
   }
-  for(auto i:res)cout<<i<<" ";//输出结果
+  for(auto i:res)cout<<i<<" ";//输出结果得到的是每k个元素的最大值，不是前k个最大值
   return res;
 }
 
